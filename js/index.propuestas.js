bee6607a4f23d48f255ca9c803b807c0aa594f6b
Gabriel Borgonovo <gabrielalejandroborgonovo@gmail.com>
2023-05-31 20:13:49 -0300

    Haciendo asyncrono el renderizado de propuestas

--- js/index.propuestas.js
+++ js/index.propuestas.js
@@ -8,17 +8,22 @@ const d = document;
 d.addEventListener('DOMContentLoaded', () => {
     showMenu();
     showMoreText();
-    renderPropuestas('./js/propuestas/ambientales.json');
-    renderPropuestas('./js/propuestas/cultura.json');
-    renderPropuestas('./js/propuestas/deportes.json');
-    renderPropuestas('./js/propuestas/economia.json');
-    renderPropuestas('./js/propuestas/educacion.json');
-    renderPropuestas('./js/propuestas/laboral.json');
-    renderPropuestas('./js/propuestas/salud.json');
-    renderPropuestas('./js/propuestas/seguridad.json');
-    renderPropuestas('/js/propuestas/servicios.json');
-    renderPropuestas('/js/propuestas/transparencia.json');
-    renderPropuestas('/js/propuestas/urbanizacion.json');
-    renderPropuestas('/js/propuestas/carta.json');
-    renderPropuestas('/js/propuestas/juventud.json');
+
+    async function render() {
+    await renderPropuestas('./js/propuestas/ambientales.json');
+    await renderPropuestas('./js/propuestas/cultura.json');
+    await renderPropuestas('./js/propuestas/deportes.json');
+    await renderPropuestas('./js/propuestas/economia.json');
+    await renderPropuestas('./js/propuestas/educacion.json');
+    await renderPropuestas('./js/propuestas/laboral.json');
+    await renderPropuestas('./js/propuestas/salud.json');
+    await renderPropuestas('./js/propuestas/seguridad.json');
+    await renderPropuestas('/js/propuestas/servicios.json');
+    await renderPropuestas('/js/propuestas/transparencia.json');
+    await renderPropuestas('/js/propuestas/urbanizacion.json');
+    await renderPropuestas('/js/propuestas/carta.json');
+    await renderPropuestas('/js/propuestas/juventud.json');
+    }
+
+    render();
 });
