99948a671b7426827c51e7b6f1c0055a5cab03f4
Gabriel Borgonovo <gabrielalejandroborgonovo@gmail.com>
2023-05-31 20:05:30 -0300

    Todas las propuestas cargadas

--- js/index.propuestas.js
+++ js/index.propuestas.js
@@ -8,6 +8,17 @@ const d = document;
 d.addEventListener('DOMContentLoaded', () => {
     showMenu();
     showMoreText();
-    renderPropuestas('./js/propuestas/seguridad.json');
+    renderPropuestas('./js/propuestas/ambientales.json');
+    renderPropuestas('./js/propuestas/cultura.json');
+    renderPropuestas('./js/propuestas/deportes.json');
+    renderPropuestas('./js/propuestas/economia.json');
+    renderPropuestas('./js/propuestas/educacion.json');
+    renderPropuestas('./js/propuestas/laboral.json');
     renderPropuestas('./js/propuestas/salud.json');
+    renderPropuestas('./js/propuestas/seguridad.json');
+    renderPropuestas('/js/propuestas/servicios.json');
+    renderPropuestas('/js/propuestas/transparencia.json');
+    renderPropuestas('/js/propuestas/urbanizacion.json');
+    renderPropuestas('/js/propuestas/carta.json');
+    renderPropuestas('/js/propuestas/juventud.json');
 });
--- js/render.propuestas.js
+++ js/render.propuestas.js
@@ -3,26 +3,25 @@ const d = document;
 const pintarPropuestas = (data) => {
     const $template = d.getElementById("template-propuestas").content,
         $fragment = d.createDocumentFragment(),
-        $contienePropuestas = d.getElementById("propuestas");
+        $contienePropuestas = d.getElementById("propuestas")
         
     //console.log('data', data.title);
     const $propuestasTitle = d.createElement('h2');
     $propuestasTitle.innerHTML = data.title;
-    $propuestasTitle.classList.add("list-title");
+    $propuestasTitle.classList.add("prop-h2");
     $fragment.appendChild($propuestasTitle);
 
     const $img = d.createElement('img');
     $img.setAttribute("src", data.img);
-    $img.classList.add("img-propuestas")
-    $fragment.appendChild($img)
+    $img.classList.add("img-propuestas");
+    $fragment.appendChild($img);
 
     data.propuestas.forEach(elemento => {
-        $template.querySelector(".propuestas-container .prop-p").textContent = elemento;
+        $template.querySelector(".prop-p").textContent = elemento;
        
         const clone = $template.cloneNode(true);
         $fragment.appendChild(clone);
     });
-
 
     $contienePropuestas.appendChild($fragment);
 }
